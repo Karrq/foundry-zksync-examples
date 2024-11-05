7975b2dc2953c7f92c55bed6220351ab03eea5a9
Francesco Dainese <franci.dainese@gmail.com>
2024-11-05 17:20:52 +0100

    feat: `Maths` and `Counter`

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Maths} from "./Maths.sol";
+
 contract Counter {
     uint256 public number;
 
@@ -9,6 +11,6 @@ contract Counter {
     }
 
     function increment() public {
-        number++;
+        number = Maths.add(number, 1);
     }
 }
